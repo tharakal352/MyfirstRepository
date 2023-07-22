707b5c6b1dafc75091872b663c5af628ce0aa89d
Tharaka Lakshan <126232613+tharakal352@users.noreply.github.com>
2023-07-22 10:35:20 +0530

    Add files via upload
    c++

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+
+int main()   //DO NOT change the 'main' signature
+{
+    string name1, name2;
+    cin >> name1;
+    cin >> name2;
+    cout << "Enter two strings:"<< endl;
+    cout << name1 << " was written by" << name2;
+    
+    return 0;
+    
+    
+    
+}
